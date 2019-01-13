223b9cc61258e1fabd9d6c8781eac4e7d29c5e13
Łukasz <lukasz.jarema@live.cim>
2019-01-13 12:29:21 +0100

    exc 3 done

--- src/main/java/zadaniaPodstawowe/exc_3/Exc_3.java
+++ src/main/java/zadaniaPodstawowe/exc_3/Exc_3.java
@@ -1,4 +1,13 @@
 package zadaniaPodstawowe.exc_3;
 
+/**
+ * Przygotuj program, który wyświetli na konsoli liczby od 1 do 49. Użyj w tym celu pętli
+ * for.
+ */
 public class Exc_3 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 49; i++) {
+      System.out.println("Liczba " + (i+1));
+    }
+  }
 }
