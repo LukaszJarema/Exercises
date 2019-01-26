936bfc17bd2fe6ec8963e8f933e1f7df87875c27
Łukasz <lukasz.jarema@live.cim>
2019-01-26 19:52:14 +0100

    Excercise 11 from zadania01/dataiCzas done

---
+++ src/main/java/zadania01/dataiCzas/Exc_11.java
@@ -0,0 +1,13 @@
+package zadania01.dataiCzas;
+
+import java.time.LocalDateTime;
+
+/**
+ * Wykorzystując obiekt typu LocalDateTime wyświetl aktualną datę i godzinę.
+ */
+
+public class Exc_11 {
+  public static void main(String[] args) {
+    System.out.println("Aktualna data i godzina: " + LocalDateTime.now());
+  }
+}
