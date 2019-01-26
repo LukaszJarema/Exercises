3b5f5d0c7104919cc25485727a4941a6a5bb0fbd
Łukasz <lukasz.jarema@live.cim>
2019-01-26 19:41:22 +0100

    Excercise 9 from zadania01/dataiCzas done

---
+++ src/main/java/zadania01/dataiCzas/Zad_9.java
@@ -0,0 +1,13 @@
+package zadania01.dataiCzas;
+
+import java.time.LocalTime;
+
+/**
+ * Wykorzystując obiekt typu LocalTime wyświetl aktualny czas.
+ */
+
+public class Zad_9 {
+  public static void main(String[] args) {
+    System.out.println("Aktualny czas: " + LocalTime.now());
+  }
+}
